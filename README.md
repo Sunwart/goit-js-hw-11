413064a7bb9e7d1b0039d71d67994e4b83a8214a
Sunwart <li.sa.vectorart@gmail.com>
2021-11-16 19:56:22 +0200

    main task completed

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # goit-js-hw-11
+
 JS Home Work # 11
---
+++ src/js/imagesAPIService.js
@@ -0,0 +1,46 @@
+import axios from 'axios';
+import SimpleLightbox from 'simplelightbox';
+
+export default class imagesAPIService {
+  constructor() {
+    this.searchQuery = '';
+    this.page = 1;
+    this.totalHits = 0;
+  }
+
+  async getImages() {
+    const url = 'https://pixabay.com/api/?key=24356494-65e5de300274261a131c8d68e';
+
+    const images = await axios.get(
+      `${url}&q=${this.searchQuery}&image_type=photo&orientation=horizontal&safesearch=true&page=${this.page}&per_page=40`,
+    );
+
+    this.totalHits = images.data.totalHits;
+
+    return images;
+  }
+
+  get query() {
+    return this.searchQuery;
+  }
+
+  set query(newQuery) {
+    this.searchQuery = newQuery;
+  }
+
+  incrementPage() {
+    this.page += 1;
+  }
+
+  resetPage() {
+    this.page = 1;
+  }
+
+  createGallery() {
+    this.gallery = new SimpleLightbox('.gallery a');
+  }
+
+  updateGallery() {
+    this.gallery.refresh();
+  }
+}
